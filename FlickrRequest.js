172250ea4a338ec9087049ab4a321822739a89f5
niklasm86 <niklasmagnusson123@gmail.com>
2015-06-08 15:43:51 +0200

    3rd commit 8/6-15 nm

--- FlickrRequest.js
+++ FlickrRequest.js
@@ -1,35 +1,36 @@
+// Niklas Magnusson, 8/6 2015, niklasmagnusson123@gmail.com
+// Flickr Picture Game arbetsprov
+//
 // JavaScript Document
 //---------------------------------------------------
 //  JS Class to search for a picture with flickrs REST API
 //---------------------------------------------------
-var FlickrRequest = {
-	xmlhttp : null,
-	picArray : [],
-	xmlDoc : null,
-
-	searchForPic : function(s){
-		FlickrRequest.picArray.splice(0, FlickrRequest.picArray.length);
-
-		FlickrRequest.xmlhttp = new XMLHttpRequest();
-		var url="https://api.flickr.com/services/rest/?method=flickr.photos.search&text="
-		+escape(s)+"&per_page=20&api_key=e4250121b37f5a01ae17b9c9a784f2fc&secret=fb59db20999c2533";
-		FlickrRequest.xmlhttp.open("GET",url,false);
-		FlickrRequest.xmlhttp.send();
-
-		if (FlickrRequest.xmlhttp.responseXML.getElementsByTagName("photo").length > 0){
-			FlickrRequest.xmlDoc = FlickrRequest.xmlhttp.responseXML;
-			for (i = 0; i < FlickrRequest.xmlDoc.getElementsByTagName("photo").length; i++) {
-					var photo = FlickrRequest.xmlDoc.getElementsByTagName("photo")[i];
-					var picId = photo.getAttributeNode('id').nodeValue;
-					var farm = photo.getAttributeNode('farm').nodeValue;
-					var server = photo.getAttributeNode('server').nodeValue;
-					var secret = photo.getAttributeNode('secret').nodeValue;
-			    	FlickrRequest.picArray.push("http://farm"+farm+".static.flickr.com/"+server+"/"+picId+"_"+secret+"_z.jpg");
-			}
-		}
-		return FlickrRequest.picArray;
-
-
-	}
+var FlickrRequest = function() {
+	self = this;
+	this.picArray = [];
 }
-
+//Function to search for a picture(tag) equal to the given parameter
+FlickrRequest.prototype.searchForPic = function(s) {
+	FlickrRequest.call(this);
+	//Empty the array to avoid old pics
+	self.picArray.splice(0, self.picArray.length);
+	var xmlhttp = new XMLHttpRequest();
+	//url containing my api key to flickr
+	var url="https://api.flickr.com/services/rest/?method=flickr.photos.search&text="
+	+escape(s)+"&per_page=20&api_key=e4250121b37f5a01ae17b9c9a784f2fc&secret=fb59db20999c2533";
+	xmlhttp.open("GET",url,false);
+	xmlhttp.send();
+	//if the responce containing more then 1 pic, push url into array
+	if (xmlhttp.responseXML.getElementsByTagName("photo").length > 0){
+		var xmlDoc = xmlhttp.responseXML;
+		for (i = 0; i < xmlDoc.getElementsByTagName("photo").length; i++) {
+			var photo = xmlDoc.getElementsByTagName("photo")[i];
+			var picId = photo.getAttributeNode('id').nodeValue;
+			var farm = photo.getAttributeNode('farm').nodeValue;
+			var server = photo.getAttributeNode('server').nodeValue;
+			var secret = photo.getAttributeNode('secret').nodeValue;
+	    	self.picArray.push("http://farm"+farm+".static.flickr.com/"+server+"/"+picId+"_"+secret+"_z.jpg");
+		}
+	}
+	return self.picArray;
+}
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+// Niklas Magnusson, 8/6 2015, niklasmagnusson123@gmail.com
+// Flickr Picture Game arbetsprov
+//
+// JavaScript Document
+//---------------------------------------------------
+//  Mainclass for the flickr picture gallery
+//---------------------------------------------------
 var Main = {
 	searchTxt : null,
 	showGallery : false,
+	flikrReq : new FlickrRequest(),
 
 	init : function(){
 		Main.initEventListeners();
 	},
-	
+
+	//init event listeners
 	initEventListeners : function(){
 		document.getElementById('btn-submit').addEventListener('click', Main.newFlickrReq);
 		document.getElementById('btn-add-gallery').addEventListener('click', Main.buildGallery);
@@ -13,19 +22,29 @@ var Main = {
 		document.getElementById('btn-clear-gallery').addEventListener('click', Main.ClearGallery);
 	},
 
+	//Clear gallery pictures
 	ClearGallery : function(){
 		var gallDiv = document.getElementById('GalleryDiv');
 		Main.disposePicsFromDiv(gallDiv);
 	},
 
+	//removes old pictures from "imageDiv" and makes a new flickr request
 	newFlickrReq : function(){
 		if (Main.hasClass(document.getElementById('GalleryDiv'),'show-gallery-pic')) document.getElementById('GalleryDiv').removeAttribute('class', 'GalleryDiv');
 		Main.disposeGalleryPics();
 		Main.disposePicsFromDiv(document.getElementById("imageDiv"));
 		Main.searchTxt = document.getElementById("search-txt").value;
-		Main.buildPics(FlickrRequest.searchForPic(Main.searchTxt));
+		Main.buildPics(Main.flikrReq.searchForPic(Main.searchTxt));
 	},
 
+	//function to remove the pictires from "imageDiv"
+	disposePicsFromDiv : function(images){
+		while (images.firstChild) {
+    		images.removeChild(images.firstChild);
+		}
+	},
+
+	//adds the result (the pictures) to the site, adding eventlistener (toggle size when clicked) to every picture
 	buildPics : function(picsArray){
 		for (i = 0; i < picsArray.length; i++) {
 			var img = document.createElement("IMG");
@@ -36,13 +55,9 @@ var Main = {
 		}
 	},
 
-	disposePicsFromDiv : function(images){
-		while (images.firstChild) {
-    		images.removeChild(images.firstChild);
-		}
-	},
-
+	//Toggle the class for pictures when clicked
 	toggleChoosenPic : function(){
+		//if the picture clicked are inside the gallery (a choosen pic)
 		if(!Main.hasClass(this.parentNode, 'Gallery')){
 			if(Main.hasClass(this, 'addImg')){
 				this.removeAttribute('class', 'addImg');
@@ -52,7 +67,9 @@ var Main = {
 				this.setAttribute('class', 'addImg');
 			}
 		}
+		//if the picture clicked are not inside the gallery
 		else{
+			//if i click a picture, minimize all others and enlarge it
 			if(!Main.hasClass(this, 'expand-single-gallery-pic') && Main.showGallery == true){
 				Main.minimizeAllPicsIsGallery();
 				this.setAttribute('class', 'expand-single-gallery-pic');
@@ -64,6 +81,7 @@ var Main = {
 		
 	},
 
+	//minimize all pictures
 	minimizeAllPicsIsGallery : function(){
 		var gal = document.getElementById('GalleryDiv').children;
 		for (i = 0; i < gal.length; i++) {
@@ -71,10 +89,12 @@ var Main = {
 		}
 	},
 
+	//My function to termen a class for an element
 	hasClass : function(element, className){
     	return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
 	},
 
+	//get all the chossen pictures to add to the gallery
 	buildGallery : function(){
 		var array = document.getElementsByClassName('addImg');
 			for (i = 0; i < array.length; i++) {
@@ -82,10 +102,12 @@ var Main = {
 			}
 	},
 
+	//Puts the choosen pictures on site
 	addImgToGallery : function(imgas){
 		document.getElementById("GalleryDiv").appendChild(imgas);
 	},
 
+	//removes the gallery pictures
 	disposeGalleryPics : function(){
 		var imageDiv = document.getElementById('imageDiv');
 		while (imageDiv.firstChild) {
@@ -94,6 +116,7 @@ var Main = {
 		
 	},
 
+	//enables the gallery mode
 	playGallery : function(){
 		Main.showGallery = true;
 		document.getElementById('GalleryDiv').setAttribute('class', 'show-gallery-pic Gallery');
